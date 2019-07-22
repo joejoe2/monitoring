609b3160b4c0f25c8d016ef01de83ce3e3810448
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2019-07-22 16:52:40 +0800

    Add files via upload

---
+++ webserver/commit.php
@@ -0,0 +1,26 @@
+<?php
+ $user="root";
+ $pass="a43235167";
+ $DBname="data";
+ 
+ $target=$_POST['target'];
+ $gatewayid=$_POST['gatewayid'];
+ $status=$_POST['status'];
+ $time=$_POST['time'];
+ $obj=$_POST['obj'];
+
+ $link=new MySQLi($_SERVER['SERVER_NAME'],$GLOBALS['user'],$GLOBALS['pass'],$DBname);
+ if($link->connect_error){ echo "error!";}
+ $s="INSERT INTO "."$target"."(id,status,time,value) VALUES('"."$gatewayid"."','"."$status"."','"."$time"."','"."$obj"."')";
+//  $link->query("set names utf8");
+  $link->query($s);
+  
+  $c=$link->query("SELECT COUNT(*) FROM "."$target"."")->fetch_row();
+  if ($c[0]>5) {
+    $result=$link->query("SELECT * from "."$target"." ORDER BY time"); 
+    $r1=$result->fetch_row();
+    $link->query("DELETE FROM "."$target"." WHERE time='"."$r1[2]"."'");
+    //echo "del".$r1[2];
+  }
+  //echo "$c[0]";
+?>
---
+++ webserver/testjson.php
@@ -0,0 +1,10 @@
+<?php
+ $user="root";
+ $pass="a43235167";
+ $DBname="data";
+ $link=new MySQLi($_SERVER['SERVER_NAME'],$GLOBALS['user'],$GLOBALS['pass'],$DBname);
+ if($link->connect_error){ echo "error!";}
+ $result=$link->query("SELECT * from "."gateway01"." ORDER BY time DESC"); 
+ $r1=$result->fetch_row();
+ echo "".$r1[0].";".$r1[1].";".$r1[2].";".$r1[3]."";
+?>
