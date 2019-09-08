149742f926ec1246c72570c0ff2e5d750c869a86
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2019-09-08 10:22:58 +0800

    Add files via upload

--- webserver/commit.php
+++ webserver/commit.php
@@ -4,19 +4,21 @@
  $DBname="data";
  
  $target=$_POST['target'];
- $gatewayid=$_POST['gatewayid'];
+ $devicesid=$_POST['devicesid'];
  $status=$_POST['status'];
  $time=$_POST['time'];
  $obj=$_POST['obj'];
+ 
+ $hold_limit=5;
 
  $link=new MySQLi("localhost",$GLOBALS['user'],$GLOBALS['pass'],$DBname);
  if($link->connect_error){ echo "error!";}
- $s="INSERT INTO "."$target"."(id,status,time,value) VALUES('"."$gatewayid"."','"."$status"."','"."$time"."','"."$obj"."')";
+ $s="INSERT INTO "."$target"."(id,status,time,value) VALUES('"."$devicesid"."','"."$status"."','"."$time"."','"."$obj"."')";
 //  $link->query("set names utf8");
   $link->query($s);
   
   $c=$link->query("SELECT COUNT(*) FROM "."$target"."")->fetch_row();
-  if ($c[0]>5) {
+  if ($c[0]>$hold_limit) {
     $result=$link->query("SELECT * from "."$target"." ORDER BY time"); 
     $r1=$result->fetch_row();
     $link->query("DELETE FROM "."$target"." WHERE time='"."$r1[2]"."'");
