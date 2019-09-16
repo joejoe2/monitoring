7ce2df30fee3218eabc91ab2cefccccaf3380526
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2019-09-16 14:09:26 +0800

    Add files via upload

--- SimulateGateways/src/simulategateways/SimulateGateways.java
+++ SimulateGateways/src/simulategateways/SimulateGateways.java
@@ -16,18 +16,28 @@ import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
 /**
  *
  * @author 70136
  */
 public class SimulateGateways {
-    static String server="219.91.49.21";
+    static String server="showdata.nctu.me";
     static int entry=5000;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String js="[{\"id\": \"sensor01\", \"type\": \"tm\", \"value\": "+(12.5)+", \"status\": \"test\"}, {\"id\": \"sensor02\", \"type\": \"tm\", \"value\":"+(100)+", \"status\": \"test\"}]";
+        JSONArray obj=new JSONArray(js);
+        System.out.println(obj.length());
+        JSONObject o1=(JSONObject)obj.getJSONObject(0);
+        o1.put("status", "runing");
+        System.out.println(obj.get(0));
+        System.out.println(obj.get(1));
+        
         
         Random random=new Random();
         Timer timer1=new Timer(5000,(e) -> {
@@ -37,7 +47,7 @@ public class SimulateGateways {
                 OutputStream outputStream=socket.getOutputStream();
                 PrintWriter printWriter=new PrintWriter(outputStream, true);
                 String json="[{\"id\": \"sensor01\", \"type\": \"tm\", \"value\": "+(random.nextFloat()*100)+", \"status\": \"test\"}, {\"id\": \"sensor02\", \"type\": \"tm\", \"value\":"+(random.nextFloat()*100)+", \"status\": \"test\"}]";
-                String urlParameters ="target=gateway01&gatewayid=00&status=test&time="+LocalDateTime.now().toString()+"&obj="+json;
+                String urlParameters ="target=devices01&devicesid=01&status=normal&time="+LocalDateTime.now().toString()+"&obj="+json;
                 printWriter.println(urlParameters);
                 printWriter.flush();
                 printWriter.close();
