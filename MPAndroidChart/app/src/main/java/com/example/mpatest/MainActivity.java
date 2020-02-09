0b78c80d3efe42a6d828d7adff99065988765a4a
F74066307 <markchen565@gmail.com>
2020-02-09 18:51:50 +0800

    add MPAtest

---
+++ MPAndroidChart/app/src/main/java/com/example/mpatest/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.mpatest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    final Handler handler = new Handler();
+    private boolean wait_time = true;
+    LineChart mChart;
+    int i=0;
+
+    public void wait_time(){
+        wait_time = false;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                wait_time = true;
+            }
+        }, 1000);
+    }
+
+    private void initChart() {
+        // enable description text
+        mChart.getDescription().setEnabled(false);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+       // l.setTypeface(mTfLight);
+        l.setTextColor(Color.WHITE);
+        l.setEnabled(false);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xl.setDrawLabels(false);
+        xl.setGranularity(1f);
+        //xl.setTypeface(mTfLight);
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.enableGridDashedLine(10f, 10f, 0f);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        //leftAxis.setTypeface(mTfLight);
+        leftAxis.setGranularity(1f);
+        leftAxis.setTextColor(Color.parseColor("#A2A2A2"));
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        //重置y轴的最小最大值   这样会让曲线随着自己的数据变化而变化
+        leftAxis.resetAxisMinimum();
+        leftAxis.resetAxisMaximum();
+
+        leftAxis.setDrawGridLines(true);
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+    }
+
+    private void addEntry() {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+            data.addEntry(new Entry(set.getEntryCount(), (float)Math.random()*1000), 0);
+            data.notifyDataChanged();
+            mChart.notifyDataSetChanged();
+            // 折线图最多显示的数量
+            mChart.setVisibleXRangeMaximum(120);
+
+            mChart.moveViewToX(data.getEntryCount());
+            wait_time();
+        }
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(Color.parseColor("#FDC328"));
+        set.setCircleColor(Color.WHITE);
+        set.setLineWidth(2f);
+        set.setCircleRadius(3f);
+        set.setDrawCircles(false);
+        set.setFillAlpha(65);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(9f);
+        set.setDrawValues(false);
+        return set;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        mChart = (LineChart) findViewById(R.id.LineChart);
+        initChart();
+        while(true) {
+            addEntry();
+        }
+    }
+}
